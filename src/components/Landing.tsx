bf2253d8e5ce5b9d5e2921652ff291e63a0be685
AtharvPatil-kit <atharvapatil808@gmail.com>
2026-03-18 16:49:39 +0530

    Add Landing component with user name

---
+++ src/components/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Atharv Patil</h1>
+    </div>
+  );
+};
+
+export default Landing;
